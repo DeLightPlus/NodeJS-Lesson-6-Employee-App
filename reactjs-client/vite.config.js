d92d2b1eb508c052e21c924b16ff603f31231423
DeLightPlus <matlakalakabelo1@gmail.com>
2024-11-10 16:45:53 +0200

    attempt deploy #2

--- reactjs-client/vite.config.js
+++ reactjs-client/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
